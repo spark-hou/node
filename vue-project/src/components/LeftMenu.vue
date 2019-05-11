5edc990b36d8e083e160e9d0cf2b2e946676bfad
1 <1>
2019-05-11 09:57:19 +0800

    511

--- vue-project/src/components/LeftMenu.vue
+++ vue-project/src/components/LeftMenu.vue
@@ -14,31 +14,24 @@
                     <i class="el-icon-menu"></i>
                     <span>商品管理</span>
                 </template>
-                <el-menu-item index="/goods/category">
-                    商品分类
-                </el-menu-item>
+                <el-menu-item index="/goods/category">商品分类</el-menu-item>
                 <el-menu-item index="/goods/release">发布商品</el-menu-item>
                 <el-menu-item index="/goods/list">商品列表</el-menu-item>
             </el-submenu>
             <el-submenu index="2">
                 <template slot="title">
-                    <i class="el-icon-menu"></i>
-                    <span>商品管理</span>
+                    <i class="el-icon-s-custom"></i>
+                    <span>用户管理</span>
                 </template>
-                <el-menu-item index="2-1">
-                    商品分类
-                </el-menu-item>
+                <el-menu-item index="2-1">用户列表</el-menu-item>
                 <el-menu-item index="2-2">发布商品</el-menu-item>
-                <el-menu-item index="2-3">商品列表</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
                 <template slot="title">
-                    <i class="el-icon-menu"></i>
-                    <span>商品管理</span>
+                    <i class="el-icon-notebook-2"></i>
+                    <span>订单管理</span>
                 </template>
-                <el-menu-item index="3-1">
-                    商品分类
-                </el-menu-item>
+                <el-menu-item index="3-1">订单列表</el-menu-item>
                 <el-menu-item index="3-2">发布商品</el-menu-item>
                 <el-menu-item index="3-3">商品列表</el-menu-item>
             </el-submenu>
