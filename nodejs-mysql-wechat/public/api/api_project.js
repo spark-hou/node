c53600b76a1f31e39604a00bb898ee113a200a4e
1 <1>
2019-05-22 19:57:19 +0800

    522

---
+++ nodejs-mysql-wechat/public/api/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "商城API文档",
+  "title": "商城API文档",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-22T03:43:45.348Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
