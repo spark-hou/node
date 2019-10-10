553aa99b20b3921d25c7c15065421c6aca4878cf
1 <1>
2019-10-10 11:46:01 +0800

    #111

--- vue-cube-ui/src/main.js
+++ vue-cube-ui/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
